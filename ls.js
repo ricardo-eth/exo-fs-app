82ba5c162a50a372b0ae9702bcf0267f1a5b023c
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-03-25 11:54:06 +0100

    update ls.js

--- ls.js
+++ ls.js
@@ -1,12 +1,15 @@
-const { readdir } = require('fs')
+const fs = require('fs')
+
+
+if (process.argv.length !== 3) {
+  console.log(`usage: node ls.js path`)
+  process.exit(1)
+}
+
+filenames = fs.readdirSync(process.argv[2]);
   
-readdir(__dirname, (err, files) => {
-  if (err)
-    console.log(err);
-  else {
-    console.log("\nCurrent directory filenames:");
-    files.forEach(file => {
-      console.log(file);
-    })
-  }
-})
+console.log("\nCurrent directory filenames:");
+filenames.forEach(file => {
+  console.log(file);
+});
+
